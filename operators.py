e76b88e8ec23816d6f43f784f3c417bec784beff
Your Name <junaidtafader@Junaids-MacBook-Pro.local>
2024-09-30 18:45:52 -0400

    Operators HW

---
+++ operators.py
@@ -0,0 +1,10 @@
+# Junaid Tafader
+
+calc = (50 - 6 * 6) / 4
+print(calc)
+
+num = 100 is not 90
+print(num)
+
+comp = ((5 > 3) and (10 < 12)) or not (15 < 20)
+print (comp)
---
+++ variables.py
@@ -0,0 +1,19 @@
+
+## Homework
+# Write a script that:
+# - Declares a string variable `favorite_food`, an integer variable `quantity`, and a float variable `price`.
+# - Changes `favorite_food` to a different food item.
+# - Prints all variables before and after the change.
+
+
+favorite_food = "Pizza"
+quantity = 5
+price = 3.25
+
+print(favorite_food, quantity, price)
+
+favorite_food = "chips"
+quantity = 3
+price = 1.50
+
+print(favorite_food, quantity, price)
