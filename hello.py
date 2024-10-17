dc2568838cf8eb7e31eea22f24147ff267b51888
Your Name <junaidtafader@Junaids-MBP.fios-router.home>
2024-10-17 00:31:17 -0400

    csv and JSON

--- hello.py
+++ hello.py
@@ -2,3 +2,15 @@ print("Hello World!")
 
 
 
+# "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+
+def no_space(x):
+	return x.replace(" ", "")
+
+# Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+def solution(number):
+	sum = 0
+	for i in range(0, number):
+		if i % 3 == 0 or i % 5 == 0:
+			sum += i
+	return sum
